dd2a8a403f28cab7be5e509fff5a3c2c4e51054f
ang-rea-dep <erface31@gmail.com>
2020-03-26 04:39:17 +0300

    change repository

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import { store } from './store'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
-    // console.log(store)
-    /* eslint-disable no-new */
+
+/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
