8f65aa991b167778a85854847303f13cd2649456
ang-rea-dep <erface31@gmail.com>
2020-06-01 23:36:37 +0300

    fixed some problems

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,6 @@
         <h1>TodoAPP using Vue.js and Vuex</h1>
         <p>Compete Tasks: {{todos.filter(todos => todos.done == true).length}}</p>
         <p>Paused Tasks: {{todos.filter(todos => todos.done == false).length}}</p>
-
         <create-todo></create-todo>
         <todo v-for="( todo, index ) in todos" v-bind:todoitem="todo" :todo.sync="todo"></todo>
     </div>
